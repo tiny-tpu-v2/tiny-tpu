01309a1e69cc22d4d2ff46e90a502a61f44aa10b
Xander Chin <xanderchin@gmail.com>
2025-05-19 22:33:26 -0400

    systolic array staging works

--- src/systolic_array.sv
+++ src/systolic_array.sv
@@ -5,25 +5,52 @@ module systolic_array(
     input logic reset,
     input logic start,
     input logic load_weight,
+    input logic load_input,
+
     input logic [31:0] w00,
     input logic [31:0] w01,
     input logic [31:0] w10,
     input logic [31:0] w11,
-    input logic [31:0] in0,
-    input logic [31:0] in1,
-    output logic [31:0] out0,
-    output logic [31:0] out1
+    
+    input logic [31:0] a00,
+    input logic [31:0] a01,
+    input logic [31:0] a10,
+    input logic [31:0] a11,
+
+    output logic [31:0] out00,
+    output logic [31:0] out01,
+    output logic [31:0] out10,
+    output logic [31:0] out11,
+    output logic done
 );
+
+    logic [31:0] in0;
+    logic [31:0] in1;
+    logic [31:0] out0;
+    logic [31:0] out1;
 
     logic [31:0] pe00_to_pe01;
     logic [31:0] pe00_to_pe10;
     logic [31:0] pe01_to_pe11;
     logic [31:0] pe10_to_pe11;
 
+    logic [31:0] a00_reg;
+    logic [31:0] a01_reg;
+    logic [31:0] a10_reg;
+    logic [31:0] a11_reg;
+
+    logic [31:0] out00_reg;
+    logic [31:0] out01_reg;
+    logic [31:0] out10_reg;
+    logic [31:0] out11_reg;
+
+    logic pe_start;
+    reg [7:0] state;
+
     pe pe00(
         .clk(clk),
         .reset(reset),
-        .start(start),
+        .start(pe_start),
         .load_weight(load_weight),
         .weight_in(w00),
         .input_in(in0),
@@ -35,7 +62,7 @@ module systolic_array(
     pe pe01(
         .clk(clk),
         .reset(reset),
-        .start(start),
+        .start(pe_start),
         .load_weight(load_weight),
         .weight_in(w01),
         .input_in(pe00_to_pe01),
@@ -47,7 +74,7 @@ module systolic_array(
     pe pe10(
         .clk(clk),
         .reset(reset),
-        .start(start),
+        .start(pe_start),
         .load_weight(load_weight),
         .weight_in(w10),
         .input_in(in1),
@@ -59,7 +86,7 @@ module systolic_array(
     pe pe11(
         .clk(clk),
         .reset(reset),
-        .start(start),
+        .start(pe_start),
         .load_weight(load_weight),
         .weight_in(w11),
         .input_in(pe10_to_pe11),
@@ -69,7 +96,107 @@ module systolic_array(
     );
 
     always @(posedge clk or posedge reset) begin
-        
+        if (reset) begin
+            // set output registers to 0
+            out00 <= 0;
+            out01 <= 0;
+            out10 <= 0;
+            out11 <= 0;
+
+            // set internal registers to 0
+            in0 <= 0;
+            in1 <= 0;
+            a00_reg <= 0;
+            a01_reg <= 0;
+            a10_reg <= 0;
+            a11_reg <= 0;
+
+            out00_reg <= 0;
+            out01_reg <= 0;
+            out10_reg <= 0;
+            out11_reg <= 0;
+
+            state <= 0;
+            pe_start <= 0;
+            done <= 0;
+        end
+        else begin
+            if(load_input) begin
+                a00_reg <= a00;
+                a01_reg <= a01;
+                a10_reg <= a10;
+                a11_reg <= a11;
+            end
+
+            if (start && state == 0) begin
+                in0 <= a00_reg;
+                in1 <= 0;
+                state <= 1;
+                pe_start <= 1;
+            end
+
+            case (state)
+                1: begin
+                    in0 <= a10_reg;
+                    in1 <= a01_reg;
+                    state <= 2;
+                end
+                2: begin
+                    in0 <= 0;
+                    in1 <= a11_reg;
+                    
+                    state <= 3;
+                end
+                3: begin
+                    in0 <= 0;
+                    in1 <= 0;
+                    out00_reg <= out0;
+                    
+                    state <= 4;
+                    
+                end
+                4: begin
+                    in0 <= 0;
+                    in1 <= 0;
+                    out10_reg <= out0;
+                    out01_reg <= out1;
+                    
+                    state <= 5;
+                end
+                5: begin
+                    in0 <= 0;
+                    in1 <= 0;
+                    out11_reg <= out1;
+                    
+                    state <= 6;
+                end
+                6: begin
+                    out00 <= out00_reg;
+                    out01 <= out01_reg;
+                    out10 <= out10_reg;
+                    out11 <= out11_reg;
+                    done <= 1;
+                    
+                    state <= 0;
+                    pe_start <= 0;
+
+                    // set internal registers to 0
+                    in0 <= 0;
+                    in1 <= 0;
+                    a00_reg <= 0;
+                    a01_reg <= 0;
+                    a10_reg <= 0;
+                    a11_reg <= 0;
+
+                    out00_reg <= 0;
+                    out01_reg <= 0;
+                    out10_reg <= 0;
+                    out11_reg <= 0;
+
+                    state <= 0;
+                end
+            endcase
+        end
     end
 
 endmodule
--- tests/systolic_array_tb.py
+++ tests/systolic_array_tb.py
@@ -20,46 +20,52 @@ async def systolic_array_tb(dut):
     dut.reset.value = 1
     dut.start.value = 0
     dut.load_weight.value = 0
+    dut.load_input.value = 0
+
     dut.w00.value = 0
     dut.w01.value = 0
     dut.w10.value = 0
     dut.w11.value = 0
-    dut.in0.value = 0
-    dut.in1.value = 0
+
+    dut.a00.value = 0
+    dut.a01.value = 0
+    dut.a10.value = 0
+    dut.a11.value = 0
 
     await ClockCycles(dut.clk, 5)
 
-    # Load weights
+    # Load weights and inputs
     dut.reset.value = 0
     dut.load_weight.value = 1
+    dut.load_input.value = 1
     dut.w00.value = weight_matrix[0][0]
     dut.w01.value = weight_matrix[0][1]
     dut.w10.value = weight_matrix[1][0]
     dut.w11.value = weight_matrix[1][1]
+    dut.a00.value = matrix_a[0][0]
+    dut.a01.value = matrix_a[0][1]
+    dut.a10.value = matrix_a[1][0]
+    dut.a11.value = matrix_a[1][1]
+
     await ClockCycles(dut.clk, 1)
     dut.load_weight.value = 0
+    dut.load_input.value = 0
     dut.w00.value = 0
     dut.w01.value = 0
     dut.w10.value = 0
     dut.w11.value = 0
-
-    await ClockCycles(dut.clk, 5)
+    dut.a00.value = 0
+    dut.a01.value = 0
+    dut.a10.value = 0
+    dut.a11.value = 0
 
     # Start the systolic array
     dut.start.value = 1
+   
 
-    # Input a00 and 0
-    dut.in0.value = matrix_a[0][0]
-    dut.in1.value = 0
-    await ClockCycles(dut.clk, 1)
-
-    # Input a10 into in0 and a01 into in1
-    dut.in0.value = matrix_a[1][0]
-    dut.in1.value = matrix_a[0][1]
-    await ClockCycles(dut.clk, 1)
-
-    # Input 0 and a11
-    dut.in0.value = 0
-    dut.in1.value = matrix_a[1][1]
-    await ClockCycles(dut.clk, 10)
+    if(dut.done.value == 0):
+        await RisingEdge(dut.done)
+    
+    dut.start.value = 0
+    await ClockCycles(dut.clk, 30)
 
